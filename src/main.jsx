3bf86a439a1bb80a5c8b1278e911789cdbac9327
Brayan Antonio <112229331+brayanalmengor04@users.noreply.github.com>
2024-08-10 03:50:34 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 
 import ReactDOM from 'react-dom/client';
-import { App } from '/src/App.jsx';
-import '/src/index.css';
+import { App } from './App.jsx';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root')) 
 root.render(<App/>)
