6c3f11350f8c2b1e58a3422c08863760d21093f1
Edwin Anel González <aneh6272@gmail.com>
2024-08-10 08:59:41 -0500

    fixing path

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: './', // Ruta base relativa, útil para GitHub Pages o Netlify
-  plugins: [react()],
   base: "/brayanalmengor04.github.io/",
+  plugins: [react()],
 })
